dbfa1c18a539b22e528a415aa8c926679e6726f0
Brandon Wu <brandicast@gmail.com>
2024-05-15 16:51:20 +0800

    test for transistor

--- raspberry pi/pico/prj/test/blink.py
+++ raspberry pi/pico/prj/test/blink.py
@@ -1,12 +1,20 @@
 from machine import Pin, Timer
 import time
 
-led = Pin("LED", Pin.OUT)
+
+led = Pin(20, Pin.OUT, Pin.PULL_UP )
+
+
+
+
 tim = Timer()
 def tick(timer):
   global led
   led.toggle()
 
-tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
+#tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
+
+led.value(1)
 
 print (time.localtime())
+
