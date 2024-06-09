c29de73e531b909b107746a9980b33fdc707620a
Brandon Wu <brandicast@gmail.com>
2024-06-10 00:22:06 +0800

    revise for actual situation

--- raspberry pi/pico/prj/gate_opener/gatekeeper.py
+++ raspberry pi/pico/prj/gate_opener/gatekeeper.py
@@ -6,15 +6,14 @@ import time
 from umqtt.simple import MQTTClient
 
 EXIT = False
-is_MQTT_CONNECTED = False
 
-test_pin = Pin(0, Pin.OUT, value=1)
+is_MQTT_CONNECTED = False
+MQTT_PING_COUNTER = 600
 
 led = led = Pin("LED", Pin.OUT, value=1)
+gate_pin = Pin (15, Pin.OUT)
 
-switch_pin = Pin (14, Pin.IN, Pin.PULL_UP)
-gate_pin = Pin (16, Pin.OUT)
-
+button_pin = Pin (2, Pin.IN, Pin.PULL_UP)
 
 
 def on_mqtt_msg(topic, msg):
@@ -32,8 +31,9 @@ wifi.activate_wifi_module()
 mqtt_server="192.168.68.57"
 client_id = "pico_gate_keeper"
 topic = "brandon/iot/pico/gate"
-client = MQTTClient(client_id, mqtt_server, 1883)
+client = MQTTClient(client_id, mqtt_server, 1883, keepalive=65)
 client.set_callback(on_mqtt_msg)
+
 
 while not EXIT:
 
@@ -43,7 +43,7 @@ while not EXIT:
     if wifi.isConnected() and not is_MQTT_CONNECTED:
         try:
             print('Connecting to MQTT Broker "%s"' % (mqtt_server))
-            client.connect()
+            client.connect(False)
             print('Connected to MQTT Broker "%s"' % (mqtt_server))
             is_MQTT_CONNECTED = True
             client.subscribe(topic)
@@ -52,7 +52,7 @@ while not EXIT:
             print ("MQTT connection error : " + str(e))
 
     # below are GPIO
-    sw = switch_pin.value()
+    sw = button_pin.value()
     if sw == 0:
         gate_pin.value(1) 
     else:
@@ -64,6 +64,10 @@ while not EXIT:
             time.sleep (0.1)           
             if (led.value() != 1):
                 led.value(1)
+            if MQTT_PING_COUNTER == 0:
+                MQTT_PING_COUNTER = 600
+                client.ping()
+            MQTT_PING_COUNTER = MQTT_PING_COUNTER - 1 
         except OSError as e:
             is_MQTT_CONNECTED = False
             print (e)
